f4507ac705ff179b8722d27dd9fdb5253374f74d
Matheus Vieira <matheusvieirasto@gmail.com>
2021-05-21 23:02:15 -0300

    Change dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,6 +3,18 @@ import { NextPage } from 'next'
 // Components
 import { Layout } from '../components/Layout'
 
-const Dashboard: NextPage = () => <Layout>Dashboard</Layout>
+// Context
+import AppProvider from '../context/AppContext'
 
-export default Dashboard
+const seo = {
+  title: 'React Boilerplate - Dashboard',
+  description: 'React boilerplate with Typescript',
+}
+
+const Home: NextPage = () => (
+  <AppProvider title={seo.title} description={seo.description}>
+    <Layout>Dashboard</Layout>
+  </AppProvider>
+)
+
+export default Home
